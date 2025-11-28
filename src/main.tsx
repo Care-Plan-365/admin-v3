7f9450e2b1f100e03b7fdc4471d1b70af90f1cfb
Abimbola-Cole Balogun <olorunyomi97@gmail.com>
2025-11-28 19:10:25 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,7 @@ createRoot(document.getElementById('root')!).render(
         <PracticeContextProvider>
           <App />
         </PracticeContextProvider>
-      </HashRouter>
+      </BrowserRouter>
     </ReduxProvider>
   </StrictMode>,
 );
